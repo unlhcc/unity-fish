e1569204b824e5c40998dfa66e93f9edce22508d
enahsthezombie <enahsnalob@gmail.com>
2016-06-20 12:30:29 -0500

    Worked on VR teleportation implementation

---
+++ FishNodes/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	GameObject camRig;
+
+	void Awake () {
+		camRig = GameObject.Find ("[CameraRig]");
+	}
+
+	void Update () {
+		if(SteamVR_Controller.Input(1).GetPressDown(SteamVR_Controller.ButtonMask.Trigger) || SteamVR_Controller.Input(2).GetPressDown(SteamVR_Controller.ButtonMask.Trigger) ){
+			Ray ray;
+			RaycastHit hit;
+			Debug.DrawRay (transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.blue);
+			bool wasHit = Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out hit);
+			if(wasHit && hit.collider.gameObject.tag == "Warp"){
+				Transform warpPoint = hit.collider.gameObject.transform;
+				camRig.transform.position = warpPoint.position;
+				camRig.transform.rotation = warpPoint.rotation;
+			}
+		}
+	}
+}
